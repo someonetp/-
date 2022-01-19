e2ed304ceaf891c05a20ff6268bc5b47624b7d09
唐萍小胖子 <97074185+someonetp@users.noreply.github.com>
2022-01-19 11:51:06 +0800

    Update hhih.c

--- hhih.c
+++ hhih.c
@@ -9,6 +9,6 @@ int main()//主函数-程序的入口main 函数只能一个// int 是整型变�
 	printf("年纪%d\n",age);//%d 是输出十进制的意思，%x打印16进制。
 	printf("身高%d\n",num);//
 	printf("体重%f\n",weigh);//浮点型，打印小数
-	printf("爱好就是付佳诚 \n");//打印字符型
+	printf("爱好就是 \n");//打印字符型
 	return 0;//返回0（整型）
 }
